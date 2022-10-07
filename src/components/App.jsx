627f6c25b74f7f9687905b523de86c78f1e89449
Veronika Kovalenko <podaritebegemotika@gmail.com>
2022-10-07 20:41:06 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
+
+import Profile from "./Profile/Profile";
+import User from './Profile/user.json';
+export default function App(){
+  return <div>
+    <User 
+  avatar={user.avatar} 
+  username={user.username} 
+  tag={user.tag} 
+  location={user.location} 
+  stats={user.stats}/> </div>;
+}
