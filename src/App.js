ffe16cfa340b4a7443db55bf2b60e179ba3b96b3
Veronika Kovalenko <podaritebegemotika@gmail.com>
2022-10-16 10:26:01 +0300

    g

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import User from './components/Users/User';
-import Statistics from './components/Statistics/Statistics';
+import Statistics from './components/Statistic/Statistics';
 import FriendList from './components/FriendList/FriendList';
 import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 import userList from './data/user.json';
